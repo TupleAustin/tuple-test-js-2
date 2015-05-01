2a694e68ebed719bec93e7b29f201649174924d3
Andrew Templeton <andrw.tmplton@gmail.com>
2015-04-30 21:27:33 -0500

    V2.0.2

--- server.js
+++ server.js
@@ -8,17 +8,24 @@ var fs = require('fs');
 var http = require('http');
 
 var seed = require('./seed');
+var db = require('./fake_db');
 
 var indexFileLocation = './index.html';
 
 http.createServer(function(req, res) {
   if (req.method == 'GET' && req.url == '/') {
-    res.writeHead(200, {"Content-Type": "text/html"});
+    res.writeHead(200, {'Content-Type': 'text/html'});
     fs.createReadStream(indexFileLocation).pipe(res);
+  } else if (req.method == 'GET' && req.url == '/todos') {
+    // A sample call to the DB, should give you an idea of pattern
+    db.list(10, function(err, list) {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(list));
+    });
   } else {
     res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end('404 Not Found');
   }
 }).listen(8000);
 
-console.log("Server running at http://127.0.0.1:8000/");
+console.log('Server running at http://127.0.0.1:8000/');
